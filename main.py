116ebe9ae6714690d817184473c1d556d116c878
KeerthanReddy048 <117931100+KeerthanReddy048@users.noreply.github.com>
2026-02-07 00:40:18 +0530

    Add Netlify app URL to CORS middleware

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",   # Vite / React
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://aiforsql.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 app.include_router(router)
+
